8f9fada0b5537925c6a3528a4ab4c641b6d2f242
Deepak Kumar <erdeep64930@gmail.com>
2024-11-30 13:20:28 +0530

    error removed

--- app/components/footer/Footer.jsx
+++ app/components/footer/Footer.jsx
@@ -1,9 +1,52 @@
 import React from 'react';
-
+import { navLinks } from "@/app/constant/navLinks";
+import Image from "next/image";
+import Link from "next/link";
+import { FaPhone } from "react-icons/fa";
+import { SiGmail } from "react-icons/si";
 const Footer = () => {
   return (
     <div>
-      footer
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 p-5 border-b-2 border-t-[1px] border-pri items-center">
+        {/* logo  */}
+        <div className="flex flex-col items-center justify-center space-y-10 my-4">
+          <Image src="/trapala.png" alt="logo" width="80" height="80" className="rounded-full"/>
+          <p className="text-sec">© <span className="text-pri">All Right Reserved 2025</span></p>
+
+        </div>
+        {/* link */}
+        <div className="my-4">
+          <p className="text-2xl font-bold text-center mb-4">Pages</p>
+          <ul className="text-center space-y-8 ">
+            {navLinks.map((link,i)=>{
+              return (
+                <Link href={link.href} key={i}>
+                  <li  className=" font-semibold hover:text-gray-500  cursor-pointer">{link.name}</li>
+                </Link>
+              )
+            })}
+          </ul>
+        </div>
+        {/* contact */}
+        <div className="flex flex-col space-y-2 my-4 md:my-0 lg:-my-4 ">
+          <p className="text-2xl font-semibold text-center  ">Contact Me</p>
+
+          <div className="flex justify-between items-center p-2 ">
+            <FaPhone />
+            <p>+91-6545358725</p>
+
+          </div>
+
+          {/*  */}
+
+          <div className="flex justify-between items-center p-2 text-pri">
+          <SiGmail />
+            <p>info@trapala.com</p>
+
+          </div>
+        </div>
+
+      </div>
     </div>
   );
 }
--- app/templates/HomePage/BannerSection/BannerSection.jsx
+++ app/templates/HomePage/BannerSection/BannerSection.jsx
@@ -1,40 +1,12 @@
-"use client";
-import { gsap } from "gsap";
+
 import Wrapper from "@/app/shared/Wrapper";
 import WrapperSection from "@/app/shared/WrapperSection";
 import Image from "next/image";
 import React from "react";
 import { FaSearch } from "react-icons/fa";
-import { useEffect, useRef } from "react";
+
 const BannerSection = () => {
-    const textLinesRef = useRef([]);
-    const imageRef = useRef(null);
-  
-    useEffect(() => {
-      const tl = gsap.timeline({ defaults: { ease: "power3.out", duration: 1 } });
-  
-      // Text animations with stagger
-      tl.from(
-        textLinesRef.current,
-        {
-          opacity: 0,
-          y: 30,
-          stagger: 0.2,
-        },
-        0.5 // Start after a slight delay
-      )
-        // Image animation
-        .from(
-          imageRef.current,
-          {
-            scale: 0.8,
-            opacity: 0,
-            duration: 1.5,
-            ease: "elastic.out(1, 0.75)", // Smooth bounce effect
-          },
-          "-=1" // Overlap with text animation
-        );
-    }, []);
+
   
   return (
     <Wrapper>
@@ -43,20 +15,20 @@ const BannerSection = () => {
         <div className="flex-1">
           <div className="flex-col p-2">
             <p
-              ref={(el) => (textLinesRef.current[0] = el)}
+              
               className="bg-white text-deepPink inline-block px-5 py-3 rounded-lg font-semibold text-red-400 hover:text-red-500 shadow-md border border-red-100"
             >
               Never Stop Learning
             </p>
             <h1
-              ref={(el) => (textLinesRef.current[1] = el)}
+             
               className="font-bold text-slate-950 text-5xl py-3 leading-[75px]"
             >
               Grow up your skills by online courses with Trapala.{" "}
             </h1>
-            <p ref={(el) => (textLinesRef.current[2] = el)} className="font-semibold text-gray-500 leading-9">
+            <p  className="font-semibold text-gray-500 leading-9">
               Find your perfect tutor. Watch and Learn lessons from 200+ of the
-              world's leading thinkers and experts.{" "}
+              world leading thinkers and experts.{" "}
             </p>
             {/* this is for search course form */}
             <div className="flex bg-white border border-gray-500 rounded-lg flex-row items-center space-x-2 mt-6 w-full px-5 py-1">
@@ -83,7 +55,7 @@ const BannerSection = () => {
         {/* right */}
         <div className="flex-1 relative">
           <Image
-            ref={imageRef}
+            
             src="/bird.png"
             width="1800"
             height="980"
@@ -91,10 +63,10 @@ const BannerSection = () => {
             className="rounded-3xl shadow-2xl bg-cover bg-center"
           />
           <h1
-            ref={(el) => (textLinesRef.current[3] = el)}
+            
             className="absolute bottom-16 italic text-4xl bg-gradient-to-r from-pink-500 to-red-500 bg-clip-text left-0 right-0 text-center font-bold text-transparent"
           >
-            "The best view always comes from the top."
+            The best view always comes from the top.
           </h1>
         </div>
       </WrapperSection>
